b2c27d8395ef6d1360ad4f55b37976c54ed2c500
Daniel Iser <daniel@code-atlantic.com>
2021-07-10 01:26:14 -0400

    Add docker down script

---
+++ bin/docker-down.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Docker Developer Stack Shutting Down..."
+dockerComposeString="-f ./docker/docker-compose.yaml"
+dockerComposeString+=" -f ./docker/docker-compose.admin.yaml"
+dockerComposeString+=" -f ./docker/docker-compose.caching.yaml"
+dockerComposeString+=" -f ./docker/docker-compose.debug.yaml"
+dockerComposeString+=" -f ./docker/docker-compose.debug-wsl2.yaml"
+
+docker-compose ${dockerComposeString} down
