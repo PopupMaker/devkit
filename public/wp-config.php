d1ccfa3e84224b97dbef372107b88294d7555229
Daniel Iser <daniel@code-atlantic.com>
2021-07-12 01:08:57 -0400

    Add fix for zlib error

--- public/wp-config.php
+++ public/wp-config.php
@@ -23,3 +23,25 @@ add_action( 'admin_enqueue_scripts', function () {
 		wp_enqueue_script( 'webpack-live-reload', 'http://localhost:35729/livereload.js', [], '0.0.0', false );
 	}
 } );
+
+if ( has_action( 'shutdown', 'wp_ob_end_flush_all' ) !== false ) {
+
+	// remove original callback
+	remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+
+	/**
+	 * Add fixed callback.
+	 * 
+	 * Fixes known core issue: https://core.trac.wordpress.org/ticket/18525, https://core.trac.wordpress.org/ticket/22430
+	 * 
+	 * Error: PHP Notice:  ob_end_flush(): Failed to send buffer of zlib output compression (0)
+	 */
+	add_action( 'shutdown', function () {
+		$start = (int) ini_get( 'zlib.output_compression' );
+		$levels = ob_get_level();
+		for ( $i = $start; $i < $levels; $i++ ) {
+				ob_end_flush();
+		}
+	}, 1 );
+
+}
