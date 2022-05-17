60f010b588254c3b829608e8d19802dba22054e7
danieliser <daniel@code-atlantic.com>
2022-05-17 00:11:16 -0400

    Remove unused code

--- public/wp-config.php
+++ public/wp-config.php
@@ -10,20 +10,6 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 require_once dirname( __DIR__ ) . '/config/application.php';
 require_once ABSPATH . 'wp-settings.php';
 
-
-/**
- * Automatically handle enqueueing livereload if available.
- */
-add_action( 'admin_enqueue_scripts', function () {
-	if ( ! defined( WP_ENV ) || 'production' === WP_ENV ) {
-		return;
-	}
-
-	if ( curl_init( 'http://localhost:35729/livereload.js' ) !== false ) {
-		wp_enqueue_script( 'webpack-live-reload', 'http://localhost:35729/livereload.js', [], '0.0.0', false );
-	}
-} );
-
 if ( has_action( 'shutdown', 'wp_ob_end_flush_all' ) !== false ) {
 
 	// remove original callback
@@ -31,9 +17,9 @@ if ( has_action( 'shutdown', 'wp_ob_end_flush_all' ) !== false ) {
 
 	/**
 	 * Add fixed callback.
-	 * 
+	 *
 	 * Fixes known core issue: https://core.trac.wordpress.org/ticket/18525, https://core.trac.wordpress.org/ticket/22430
-	 * 
+	 *
 	 * Error: PHP Notice:  ob_end_flush(): Failed to send buffer of zlib output compression (0)
 	 */
 	add_action( 'shutdown', function () {
